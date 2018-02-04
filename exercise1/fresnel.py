3b837d0032db1acd9c57b6848f21cf1f9dbf3bac
brianlo747 <brianlo747@gmail.com>
2018-02-04 22:11:41 +0000

    Exercise 1 Done and Formatted

--- exercise1/fresnel.py
+++ exercise1/fresnel.py
@@ -3,19 +3,31 @@ import scipy.integrate as integration
 import matplotlib.pyplot as plt
 
 def C(x):
+    '''
+        Takes in argument x
+        Returns integrand value for C(x)
+    '''
     return np.cos((np.pi * (x ** 2))/2)
 
 def S(x):
+    '''
+        Takes in argument x
+        Returns integrand value for S(x)
+    '''
     return np.sin((np.pi * (x ** 2))/2)
 
 def integrate(f, x_lower, x_upper):
-    return integration.quad(f, x_lower, x_upper)
+    '''
+        Takes in (function, lower integration limit, upper integration limit)
+        Returns accurately integrated result
+    '''
+    return integration.quad(f, x_lower, x_upper)[0]
 
 
 points_to_plot = []
 for u1 in np.linspace(-8, 8, 3000):
-    c = integrate(C, 0, u1)[0]
-    s = integrate(S, 0, u1)[0]
+    c = integrate(C, 0, u1)
+    s = integrate(S, 0, u1)
     points_to_plot.append([c,s])
 
 c_points = [l[0] for l in points_to_plot]
@@ -24,47 +36,67 @@ s_points = [l[1] for l in points_to_plot]
 fig1, ax1 = plt.subplots()
 ax1.plot(c_points, s_points, color = 'blue', lw = 0.5)
 ax1.xaxis.set_ticks_position('bottom')
-ax1.spines['bottom'].set_position(('data',0)) # set position of x spine to x=0
+ax1.spines['bottom'].set_position(('data',0)) # set position of x spine to x = 0
 ax1.yaxis.set_ticks_position('left')
-ax1.spines['left'].set_position(('data',0))   # set position of y spine to y=0
+ax1.spines['left'].set_position(('data',0)) # set position of y spine to y = 0
 ax1.spines['right'].set_color('none')
 ax1.spines['top'].set_color('none')
+ax1.set_xlim([-0.80, 0.801])
+ax1.xaxis.set_ticks(np.arange(-0.80, 0.81, 0.20))
+ax1.set_ylim([-0.80, 0.801])
+ax1.yaxis.set_ticks(np.arange(-0.80, 0.81, 0.20))
 ax1.set_xlabel("C(u)")
 ax1.xaxis.set_label_coords(1.0, 0.55)
 ax1.set_ylabel("S(u)", rotation='horizontal')
-ax1.yaxis.set_label_coords(0.45, 0.95)
-plt.title("The Cornu Spiral")
-fig1.savefig("Core2.pdf")
+ax1.yaxis.set_label_coords(0.55, 0.95)
+ax1.set_title("The Cornu Spiral")
+ax1.set_aspect("equal")
+fig1.savefig("Core2.pdf", bbox_inches = 'tight')
 
 #########
 
 d = 10
 lamb = 1
 lengths_D_to_try = [30,50,100]
-
+tol = 1.5 # tolerance value to prevent discontinuous phase plot
 
 for D in lengths_D_to_try:
-    amplitudes_to_plot = []
-    for u2 in np.linspace(-25, 25, 5000):
-        x1 = ((d/2) - u2) * np.sqrt(2/(lamb * D))
-        x0 = (-(d/2) - u2) * np.sqrt(2/(lamb * D))
+    positions_to_plot = np.linspace(-25, 25, 3000)
+    intensities_to_plot = np.array([])
+    phases_to_plot = np.array([])
+    for u2 in positions_to_plot:
+        x1 = ((d/2) - u2) * np.sqrt(2/(lamb * D)) # lower integration limit
+        x0 = (-(d/2) - u2) * np.sqrt(2/(lamb * D)) # upper integration limit
         c = integrate(C, x0, x1)[0]
         s = integrate(S, x0, x1)[0]
         intensity = c ** 2 + s ** 2
-        amplitudes_to_plot.append([u2,intensity])
+        phase = np.arctan(s/c)
+        intensities_to_plot = np.append(intensities_to_plot, intensity)
+        phases_to_plot = np.append(phases_to_plot, phase)
 
-    positions = [l[0] for l in amplitudes_to_plot]
-    intensities = [l[1] for l in amplitudes_to_plot]
+    phases_to_plot[phases_to_plot > tol] = np.nan
+    intensities_to_plot = intensities_to_plot/max(intensities_to_plot)
 
-    fig2, ax2 = plt.subplots()
-    ax2.plot(positions, intensities, color = 'blue', lw = 0.5)
-    ax2.xaxis.set_ticks_position('bottom')
-    ax2.yaxis.set_ticks_position('left')
-    ax2.spines['left'].set_position(('data',0))   # set position of y spine to y=0
-    ax2.spines['right'].set_color('none')
-    ax2.spines['top'].set_color('none')
-    ax2.set_xlabel("Distance from centre of slit on plane normal to slit / cm")
-    ax2.set_ylabel("Intensity / arb. units", rotation='horizontal')
-    ax2.yaxis.set_label_coords(0.66, 0.95)
-    plt.title("Diffraction Pattern for D = " + str(D) + "cm")
-    fig2.savefig("Supplementary2_" + str(D) + ".pdf")
+    figs, axarr = plt.subplots(2, 1, figsize = (10,15))
+
+    axarr[0].plot(positions_to_plot, intensities_to_plot, color='red', lw=0.5)
+    axarr[0].xaxis.set_ticks_position('bottom')
+    axarr[0].yaxis.set_ticks_position('left')
+    axarr[0].spines['left'].set_position(('data', 0))
+    axarr[0].spines['right'].set_color('none')
+    axarr[0].spines['top'].set_color('none')
+    axarr[0].set_xlabel("Distance from optical axis / cm")
+    axarr[0].set_ylabel("Relative Intensity / arb. units", rotation='horizontal')
+    axarr[0].yaxis.set_label_coords(0.65, 0.95)
+    axarr[0].set_title("Diffraction Pattern for D = " + str(D) + "cm")
+    axarr[0].set_aspect("auto")
+
+    axarr[1].plot(positions_to_plot, phases_to_plot, color='blue', lw=0.5)
+    axarr[1].set_ylim([-1.5, 1.51])
+    axarr[1].yaxis.set_ticks(np.arange(-1.5, 1.51, 0.3))
+    axarr[1].set_xlabel("Distance from optical axis / cm")
+    axarr[1].set_ylabel("Relative Phase / rad")
+    axarr[1].set_title("Phase of Diffraction Pattern for D = " + str(D) + "cm")
+    axarr[1].set_aspect("auto")
+
+    figs.savefig("Supplementary2_" + str(D) + ".pdf", bbox_inches = 'tight')
--- exercise1/monte_carlo.py
+++ exercise1/monte_carlo.py
@@ -1,24 +1,42 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def integrand(x):
-    return (10**6) * np.sin(np.sum(x))
+    '''
+        Takes in a numpy array of x-values in 8 dimensions
+        Returns integrand value
+    '''
+    return (10 ** 6) * np.sin(np.sum(x))
+
 
 def monte_carlo(N):
-    eight_d_points = np.array(np.random.rand(N, 8) * (np.pi/8)) #Generates 8-D random points within range
-    integrand_of_points = np.apply_along_axis(integrand, 1, eight_d_points) #Calculates integrand for each of these points
-    f_mean = (1/N) * np.sum(integrand_of_points)
-    f_squared = (1/N) * np.sum(np.square(integrand_of_points))
-    volume = ((np.pi/8)**8)
+    '''
+        Takes in your desired number of sample points to be generated
+        Returns Monte-Carlo estimate with theoretical error
+    '''
+    eight_d_points = np.array(np.random.rand(N, 8) * (np.pi / 8))  # Generates 8-D random points within range
+    integrand_of_points = np.apply_along_axis(integrand, 1,
+                                              eight_d_points)  # Calculates integrand for each of these points
+    f_mean = (1 / N) * np.sum(integrand_of_points)
+    f_squared = (1 / N) * np.sum(np.square(integrand_of_points))
+    volume = ((np.pi / 8) ** 8)
 
     integral_estimate = volume * f_mean
-    error_estimate = volume * np.sqrt((f_squared - f_mean ** 2)/N)
+    error_estimate = volume * np.sqrt((f_squared - f_mean ** 2) / N)
 
     return [integral_estimate, error_estimate]
 
+
 def multiple_runs(N_max, sep, n_t):
-    list_of_Ns = np.array(np.logspace(2, N_max, num=sep)) #Creates logarithmacally spaced values from 2 to N_max
-    list_of_Ns = list_of_Ns.astype(int) #and converts it into an integer list
+    '''
+        Takes in (Maximum number of sample points N_max, Different Ns to be tested from 2 to N_max,
+        Number of times each N is tested)
+        Returns a list of results containing lists in the form of
+        (Sample points, Mean integral estimate, Overall Scatter, Overall Theoretical Error)
+    '''
+    list_of_Ns = np.array(np.logspace(2, N_max, num=sep))  # Creates logarithmacally spaced values from 2 to N_max
+    list_of_Ns = list_of_Ns.astype(int)  # and converts it into an integer list
     print(list_of_Ns)
     results = []
     for N in list_of_Ns:
@@ -29,10 +47,10 @@ def multiple_runs(N_max, sep, n_t):
             run = monte_carlo(N)
             mean_accu += run[0]
             error2_accu += (run[1] ** 2)
-            integrated_list = np.append(integrated_list,run[0])
+            integrated_list = np.append(integrated_list, run[0])
         print(integrated_list)
-        results.append([N, mean_accu/n_t, np.std(integrated_list)/np.sqrt(n_t), np.sqrt(error2_accu/n_t)])
-        print("N = "+str(N)+" computation complete!")
+        results.append([N, mean_accu / n_t, np.std(integrated_list) / np.sqrt(n_t), np.sqrt(error2_accu / n_t)])
+        print("N = " + str(N) + " computation complete!")
     print(results)
     return results
 
@@ -42,8 +60,8 @@ values_of_N = [l[0] for l in results_table]
 scatter_errors = [l[2] for l in results_table]
 theoretical_errors = [l[3] for l in results_table]
 
-coeffs_scatter = np.polyfit(np.log(values_of_N), np.log(scatter_errors), deg=1)
-coeffs_theoretical = np.polyfit(np.log(values_of_N), np.log(theoretical_errors), deg=1)
+coeffs_scatter = np.polyfit(np.log(values_of_N), np.log(scatter_errors), deg=1)  # Fits scatter error values
+coeffs_theoretical = np.polyfit(np.log(values_of_N), np.log(theoretical_errors), deg=1)  # Fits theoretical error values
 poly_scatter = np.poly1d(coeffs_scatter)
 poly_theoretical = np.poly1d(coeffs_theoretical)
 yfit_scatter = lambda x: np.exp(poly_scatter(np.log(x)))
@@ -55,7 +73,7 @@ print(poly_theoretical)
 plt.loglog(values_of_N, scatter_errors, 'o', markersize=2, color='blue',
            label="Standard Deviation of Monte-Carlo estimates")
 plt.loglog(values_of_N, yfit_scatter(values_of_N), color='blue',
-           label = "Standard Deviation of Monte-Carlo estimates (Best-fit)")
+           label="Standard Deviation of Monte-Carlo estimates (Best-fit)")
 plt.loglog(values_of_N, theoretical_errors, 'o', markersize=2, color='orange',
            label="Theoretical error of Monte-Carlo estimates")
 plt.loglog(values_of_N, yfit_theoretical(values_of_N), color='orange',
